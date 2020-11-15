6a8d9404bbc258091058e080a6985cac78ba658c
Shashwat Singh <shashwatsing@umass.edu>
2020-11-15 17:54:07 -0500

    added a slider

--- frontend/my-maps-project/src/app/app.module.ts
+++ frontend/my-maps-project/src/app/app.module.ts
@@ -5,19 +5,22 @@ import { AgmCoreModule } from '@agm/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DirectionsMapDirective } from './directions-map.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent,
-
     DirectionsMapDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatSliderModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyANBJfk8OsDGMa7QBR6IIzc2uJn3EqYslo'
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
