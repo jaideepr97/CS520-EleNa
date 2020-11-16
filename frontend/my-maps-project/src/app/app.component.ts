9dd0c5adbb274754be552dd5c58f7cd8b478760b
dragonblair <bkr.koushikreddy@gmail.com>
2020-11-16 10:43:06 -0500

    Added dynamic markers

--- frontend/my-maps-project/src/app/app.component.ts
+++ frontend/my-maps-project/src/app/app.component.ts
@@ -11,8 +11,13 @@ export class AppComponent {
   originLat = 42.37173098058097;
   originLng = -72.60600101123048;
 
+  
+
   destinationLat = 42.373070; 
-  destinationLng = -72.501176
+  destinationLng = -72.501176;
+
+  locations = [{lat:this.originLat, lng:this.originLng},
+    {lat:this.destinationLat, lng:this.destinationLng}]
   
   origin = { lat: 42.350489, lng: -72.527421 };
   destination = { lat: 42.451643, lng: -72.565172 };
@@ -25,6 +30,8 @@ export class AppComponent {
     console.log($event);
     this.originLat= $event.coords.lat;
     this.originLng = $event.coords.lng;
+
+    // this.locations[0] = {lat:this.originLat, lng:this.originLng}
   }
 
   endDragDestination($event: any): void {
