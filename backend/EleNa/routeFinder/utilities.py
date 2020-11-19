5501f473b6424a5a009df7d0a7bfbc9e451f8d30
Jaideep Rao <jaideeprao@MacBook-Pro.local>
2020-11-18 20:06:20 -0500

    updated requirements.txt
    Signed-off-by: Jaideep Rao <jaideeprao@MacBook-Pro.local>

--- backend/EleNa/routeFinder/utilities.py
+++ backend/EleNa/routeFinder/utilities.py
@@ -1,15 +1,15 @@
 import math
 
 def getClosestMappedNode(graph, source):
-    temp_node = None
-    min_distance = 1000000000
+    closestNode = None
+    minDistance = math.inf
     for osmid in graph.nodes:
         node = graph.nodes[osmid]
         distance = calcStraightLineDistance(source, node)
-        if distance < min_distance:
-            min_distance = distance
-            temp_node = node.osmid
-    return temp_node
+        if distance < minDistance:
+            minDistance = distance
+            closestNode = node.osmid
+    return closestNode
 
 def calcStraightLineDistance(source, destination):
     return math.sqrt( math.pow((destination.latitude - source.latitude),2) + math.pow((destination.longitude - source.longitude),2))
